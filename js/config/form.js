f342e5c2e5d7c1effcd1596853add69c2d101921
jonilsen <jonilsen@gmail.com>
2018-07-25 18:02:18 -0400

    Removed second section

--- js/config/form.js
+++ js/config/form.js
@@ -1,66 +1,60 @@
-import Introduction from '../components/Introduction.jsx';
-import fullName from '../definitions/fullName';
+import Introduction from "../components/Introduction.jsx";
+import fullName from "../definitions/fullName";
 
-import fullNameUI from 'us-forms-system/lib/js/definitions/fullName';
-import PhoneNumberWidget from 'us-forms-system/lib/js/widgets/PhoneNumberWidget';
-import phoneUI from '../definitions/phone';
+import fullNameUI from "us-forms-system/lib/js/definitions/fullName";
+import PhoneNumberWidget from "us-forms-system/lib/js/widgets/PhoneNumberWidget";
+import phoneUI from "../definitions/phone";
 
 const formConfig = {
-  title: 'Form',
-  subTitle: 'Test',
-  formId: '',
-  urlPrefix: '/',
-  trackingPrefix: 'form-',
-  transformForSubmit: '',
-  submitUrl: '',
+  title: "USCIS Electronic Notification",
+  subTitle: "of Application/Petition Acceptance",
+  formId: "G-1145",
+  urlPrefix: "/",
+  trackingPrefix: "form-",
+  transformForSubmit: "",
+  submitUrl: "",
   introduction: Introduction,
-  confirmation: '',
+  confirmation: "",
   defaultDefinitions: {
     fullName
   },
   chapters: {
     firstSection: {
-      title: 'First Section',
+      title: "Enter Applicant/Petitioner Information",
       pages: {
         firstPage: {
-          path: 'first-section/first-page',
-          title: 'First Page',
+          path: "first-section/first-page",
+          title: "First Page",
           uiSchema: {
             fullName: fullNameUI,
             emailAddress: {
-              'ui:widget': 'email',
-              'ui:title': 'Email Address'
+              "ui:widget": "email",
+              "ui:title": "Email Address"
             },
             hasMobilePhone: {
-              'ui:title': 'Do you have a U.S. Mobile Phone that can receive text messages?'
+              "ui:title":
+                "Do you have a U.S. Mobile Phone that can receive text messages?"
             },
-            phoneNumber: phoneUI('U.S. Mobile Phone Number (Message and data rates may apply)')
+            phoneNumber: phoneUI(
+              "U.S. Mobile Phone Number (Message and data rates may apply)"
+            )
           },
           schema: {
-            type: 'object',
+            type: "object",
             properties: {
               fullName,
               emailAddress: {
-                type: 'string'
+                type: "string"
               },
               hasMobilePhone: {
-                type: 'boolean'
+                type: "boolean"
               },
               phoneNumber: {
-                type: 'string',
+                type: "string",
                 minLength: 10,
                 maxLength: 15
               }
             }
-          }
-        },
-        secondPage: {
-          path: 'first-section/second-page',
-          title: 'Second Page',
-          uiSchema: {},
-          schema: {
-            type: 'object',
-            properties: {}
           }
         }
       }
